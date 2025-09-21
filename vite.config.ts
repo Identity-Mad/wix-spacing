e9bad4975ed624d60d2a0abefb400eb793142d06
beau05@gmail.com <beau05@gmail.com>
2025-09-21 09:10:55 -0500

    Update Vite configuration base path from "/wix-spacing-simulator/" to "/wix-spacing/" to reflect project restructuring.

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/wix-spacing-simulator/",
+  base: "/wix-spacing/",
   plugins: [react()],
   optimizeDeps: {
     exclude: ["lucide-react"],
